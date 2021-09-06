ba8b1b4e17735ccabda591570ebe4b8b27616ece
NimzyRocha <89158726+NimzyRocha@users.noreply.github.com>
2021-09-06 15:26:30 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    num = int (input())
+    cont = 0
+    digito = "#"
+    while num>cont:
+        cont = cont +1
+        if digito == "#":
+            print (cont,"-",digito)
+            digito = "%"
+        else:
+            if digito == "%":
+                print (cont,"-",digito)
+                digito = "#"
     pass
 
 if __name__=='__main__':   
