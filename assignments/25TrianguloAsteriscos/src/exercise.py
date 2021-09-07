0e21591162b625bf7d18646642b505d6de840d0e
NimzyRocha <89158726+NimzyRocha@users.noreply.github.com>
2021-09-06 19:41:49 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,15 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    h = int (input("Enter triangle height: "))
+    cont = h
+    contdos = 0
+    while cont != 0 :
+        cont = cont - 1
+        espacio = cont * " "
+        contdos = contdos + 1
+        asterisco = contdos * "*"
+        print (espacio,asterisco)
     pass
 
 
